111ef0cbad17acf745a3079aaef55c24c18d0ebc
Shuffle <zhirong.mark.huang@gmail.com>
2018-03-20 16:50:28 -0400

    Added Log4net to project see how this will go

--- PTC-Creator/Program.cs
+++ PTC-Creator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,12 +15,36 @@ namespace PTC_Creator
         [STAThread]
         static void Main()
         {
+            AppDomain currentDomain = default(AppDomain);
+            currentDomain = AppDomain.CurrentDomain;
+            // Handler for unhandled exceptions.
+            currentDomain.UnhandledException += GlobalUnhandledExceptionHandler;
+            // Handler for exceptions in threads behind forms.
+            Application.ThreadException += GlobalThreadExceptionHandler;
+
             Properties.Settings.Default.Upgrade();
             Properties.Settings.Default.Reload();
             Properties.Settings.Default.Save();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+
+        private static void GlobalUnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = default(Exception);
+            ex = (Exception)e.ExceptionObject;
+            ILog log = LogManager.GetLogger(typeof(Program));
+            log.Error(ex.Message + "\n" + ex.StackTrace);
+        }
+
+        private static void GlobalThreadExceptionHandler(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            Exception ex = default(Exception);
+            ex = e.Exception;
+            ILog log = LogManager.GetLogger(typeof(Program)); //Log4NET
+            log.Error(ex.Message + "\n" + ex.StackTrace);
         }
     }
 }
