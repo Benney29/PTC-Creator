f409668eb4bebb734575362bad4c0ee604dd1134
Shuffle <zhirong.mark.huang@gmail.com>
2018-03-21 01:50:29 -0400

    Write unhandled exceptions to log

--- PTC-Creator/Program.cs
+++ PTC-Creator/Program.cs
@@ -24,26 +24,42 @@ namespace PTC_Creator
             Application.Run(new MainForm());
 
             Application.ThreadException += Application_ThreadException;
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CatchUnhandledException);
         }
 
 
         private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
         {
-            if (e.Exception is ObjectDisposedException)
+            MessageBox.Show("An object disposed exception has occured. A log will be written to log.txt" + Environment.NewLine
+                + "Please submit it to github issues", "Fatal Error");
+
+            try
             {
-                MessageBox.Show("An object disposed exception has occured. A log will be written to log.txt", "Fatal Error");
-
-                try
-                {
-                    File.WriteAllText("log.txt", e.Exception.ToString());
-                }
-                catch
-                {
-                    MessageBox.Show(String.Format("Failed to write log file. Screenshot this exception:\n{0}", e.Exception.ToString()));
-                }
-
-                Application.Exit();
+                File.WriteAllText("log.txt", e.Exception.ToString());
             }
+            catch
+            {
+                MessageBox.Show(String.Format("Failed to write log file. Screenshot this exception:\n{0}", e.Exception.ToString()));
+            }
+
+            Application.Exit();
+        }
+
+        private static void CatchUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An object disposed exception has occured. A log will be written to log.txt" + Environment.NewLine
+                + "Please submit it to github issues", "Fatal Error");
+
+            try
+            {
+                File.WriteAllText("log.txt", e.ExceptionObject.ToString());
+            }
+            catch
+            {
+                MessageBox.Show(String.Format("Failed to write log file. Screenshot this exception:\n{0}", e.ExceptionObject.ToString()));
+            }
+
+            Application.Exit();
         }
     }
 }
